fe8a8592947aa263ea2e0adc72041faf3051e2b6
Aczois <theaczoisscripts@gmail.com>
2024-12-07 16:16:34 +0000

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -1,1 +1,99 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/Aczois/Aczois/Aczois/Orion/Lib')))()
 
+local Window = OrionLib:MakeWindow({Name = "Aczois", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+--a
+
+local Tab = Window:MakeTab({
+	Name = "A",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+--b
+
+local Tab = Window:MakeTab({
+	Name = "B",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+--c
+
+local Tab = Window:MakeTab({
+	Name = "C",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+--d
+
+local Tab = Window:MakeTab({
+	Name = "D",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+--e
+
+local Tab = Window:MakeTab({
+	Name = "E",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+--f
+
+local Tab = Window:MakeTab({
+	Name = "F",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+--g
+
+local Tab = Window:MakeTab({
+	Name = "G",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+--h
+
+local Tab = Window:MakeTab({
+	Name = "H",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+--i
+
+local Tab = Window:MakeTab({
+	Name = "I",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+--j
+
+local Tab = Window:MakeTab({
+	Name = "J",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+--k
+
+local Tab = Window:MakeTab({
+	Name = "K",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+--l
+
+local Tab = Window:MakeTab({
+	Name = "L",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
