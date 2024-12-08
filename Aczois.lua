e51d80eb8a58c797481a598a0cbcd780edc655c7
Aczois <theaczoisscripts@gmail.com>
2024-12-08 19:20:40 +0000

    Create Aczois.lua

---
+++ Aczois.lua
@@ -0,0 +1,3 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/Aczois/Aczois/Aczois/Orion/Lib')))()
+
+local Window = OrionLib:MakeWindow({Name = "Key System", HidePremium = false, SaveConfig = true, IntroText = "Key System"})
