3f6c132f83d0f5b45af07c635f5a310f35c85441
Aczois <theaczoisscripts@gmail.com>
2024-12-08 19:29:24 +0000

    Update Aczois.lua

--- Aczois.lua
+++ Aczois.lua
@@ -1,3 +1,9 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/Aczois/Aczois/Aczois/Orion/Lib')))()
 
 local Window = OrionLib:MakeWindow({Name = "Key System", HidePremium = false, SaveConfig = true, IntroText = "Key System"})
+
+local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
