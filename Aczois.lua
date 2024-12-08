4734e72a838fa5e65fb008ee1b430985ebce394c
Aczois <theaczoisscripts@gmail.com>
2024-12-08 19:38:25 +0000

    Update Aczois.lua

--- Aczois.lua
+++ Aczois.lua
@@ -1,9 +1,17 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/Aczois/RBX/refs/heads/main/Orion/Lib')))()
-
+local player = game.Players.LocalPlayer
 local Window = OrionLib:MakeWindow({Name = "Key System", HidePremium = false, SaveConfig = true, IntroText = "Key System"})
 
+OrionLib:MakeNotification({
+	Name = "Logged In!",
+	Content = "You are logged in as "..Player.Name..,
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
 local Tab = Window:MakeTab({
-	Name = "Tab 1",
+	Name = "Key",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
+
