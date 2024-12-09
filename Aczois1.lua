2d75395464573a571f6c3011e76394f51afb3cac
Aczois <theaczoisscripts@gmail.com>
2024-12-09 01:07:05 +0000

    Update Aczois1.lua

--- Aczois1.lua
+++ Aczois1.lua
@@ -30,6 +30,6 @@ local Window = Rayfield:CreateWindow({
       FileName = "Aczois6292Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Aczois2024T"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Key = {"https://lootdest.org/s?f5f3e1c3"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
