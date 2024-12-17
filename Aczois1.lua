0bb75ef3130b0265eda3a72fe8c59ff23da2929f
Aczois <theaczoisscripts@gmail.com>
2024-12-17 11:21:44 +0000

    Update Aczois1.lua

--- Aczois1.lua
+++ Aczois1.lua
@@ -1,16 +1,17 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/Aczois/RBX/refs/heads/main/Orion/Lib')))()
-
 local Window = OrionLib:MakeWindow({Name = "Aczois Unsimplified", HidePremium = false, SaveConfig = true, ConfigFolder = "Aczois1Test6292"})
 
 local Tab = Window:MakeTab({
-	Name = "SCP3008",
+	Name = "SCP 3008",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
 
 Tab:AddButton({
-	Name = "Get Food",
+	Name = "Button!",
 	Callback = function()
       		loadstring(game:HttpGet(("https://raw.githubusercontent.com/Aczois/RBX/refs/heads/main/Safe/SCP%203008%20Food"))()
   	end    
 })
+
+OrionLib:Init()
