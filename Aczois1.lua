11876e5d3a0844089c03d7eb17dd29763b1b972d
Aczois <theaczoisscripts@gmail.com>
2024-12-17 11:23:21 +0000

    Update Aczois1.lua

--- Aczois1.lua
+++ Aczois1.lua
@@ -1,5 +1,5 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/Aczois/RBX/refs/heads/main/Orion/Lib')))()
-local Window = OrionLib:MakeWindow({Name = "Aczois Unsimplified", HidePremium = false, SaveConfig = true, ConfigFolder = "Aczois1Test6292"})
+local Window = OrionLib:MakeWindow({Name = "Aczois Express", HidePremium = false, SaveConfig = true, ConfigFolder = "AczoisExpress6292L11"})
 
 local Tab = Window:MakeTab({
 	Name = "SCP 3008",
